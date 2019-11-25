fde0022c5831c88230e72f64e47d6220eec720af
chizhaoyang <cy0003@mix.wvu.edu>
2019-11-25 10:13:51 -0500

    update

--- subscribe_data_to_txt/include/subscribe_data_to_txt/SubscribeDataToTxt.h
+++ subscribe_data_to_txt/include/subscribe_data_to_txt/SubscribeDataToTxt.h
@@ -8,6 +8,8 @@
 #include <ros/ros.h>
 
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/MagneticField.h>
 
 class SubscribeDataToTxt {
 public:
@@ -21,14 +23,21 @@ private:
 	bool LoadParameters(const ros::NodeHandle& n);
 
 	bool RegisterCallbacks(const ros::NodeHandle& n);
-	void WritingData(const nav_msgs::Odometry& msg);
+	// Pose from vicon.
+	void WritingData1(const geometry_msgs::TransformStamped& msg);
+	// Magnetic information.
+	void WritingData2(const sensor_msgs::MagneticField& msg);
 
-	ros::Subscriber sub_topic_;
+	ros::Subscriber sub_topic1_;
+	ros::Subscriber sub_topic2_;
 
 	std::string name_;
-	std::string topic_name_;
-	std::string file_name_;
+	std::string topic_name1_;
+	std::string file_name1_;
+	std::string topic_name2_;
+	std::string file_name2_;
 
-	std::ofstream writingfile_;
+	std::ofstream writingfile1_;
+	std::ofstream writingfile2_;
 
 };
--- subscribe_data_to_txt/src/SubscribeDataToTxt.cpp
+++ subscribe_data_to_txt/src/SubscribeDataToTxt.cpp
@@ -24,10 +24,13 @@ bool SubscribeDataToTxt::Initialize(const ros::NodeHandle& n){
 }
 
 bool SubscribeDataToTxt::LoadParameters(const ros::NodeHandle& n){
-	if(!pu::Get("topic_name", topic_name_)) return false;
-	if(!pu::Get("file_name", file_name_)) return false;
+	if(!pu::Get("topic_name1", topic_name1_)) return false;
+	if(!pu::Get("topic_name2", topic_name2_)) return false;
+	if(!pu::Get("file_name1", file_name1_)) return false;
+	if(!pu::Get("file_name2", file_name2_)) return false;
 
-	writingfile_.open(file_name_.c_str());
+	writingfile1_.open(file_name1_.c_str());
+	writingfile2_.open(file_name2_.c_str());
 
 	return true;
 }
@@ -37,16 +40,30 @@ bool SubscribeDataToTxt::RegisterCallbacks(const ros::NodeHandle& n){
 	ros::NodeHandle nl(n);	// create local node
 
 	// if need more than one topic, copy this line.
-	sub_topic_ = nl.subscribe(topic_name_, 100, &SubscribeDataToTxt::WritingData, this);
+	sub_topic1_ = nl.subscribe(topic_name1_, 100, &SubscribeDataToTxt::WritingData1, this);
+	sub_topic2_ = nl.subscribe(topic_name2_, 100, &SubscribeDataToTxt::WritingData2, this);
 
 	return true;
 }
 
 // need modify this function to save specific variables
-void SubscribeDataToTxt::WritingData(const nav_msgs::Odometry& msg){
+// void SubscribeDataToTxt::WritingData(const nav_msgs::Odometry& msg){
 
-	writingfile_ << msg.header.stamp << " " << msg.header.frame_id << " " <<
-	msg.pose.pose.position.x << " " << msg.pose.pose.position.y << " " <<
-	msg.pose.pose.position.z << "\n";
+// 	writingfile_ << msg.header.stamp << " " << msg.header.frame_id << " " <<
+// 	msg.pose.pose.position.x << " " << msg.pose.pose.position.y << " " <<
+// 	msg.pose.pose.position.z << "\n";
 
+// }
+
+void SubscribeDataToTxt::WritingData1(const geometry_msgs::TransformStamped& msg){
+	
+	writingfile1_ << msg.header.stamp << " " << msg.header.frame_id << " " <<
+	msg.child_frame_id << " " << msg.transform.translation.x << " " << 
+	msg.transform.translation.y << " " << msg.transform.translation.z << "\n";
+}
+
+void SubscribeDataToTxt::WritingData2(const sensor_msgs::MagneticField& msg){
+
+	writingfile2_ << msg.header.stamp << " " << msg.magnetic_field.x << " " <<
+	msg.magnetic_field.y << " " << msg.magnetic_field.z << "\n";
 }
